fdab1b152921072200afb290bc25f1a9a673551b
Aman <aman.swar001@gmail.com>
2025-08-22 13:19:59 +0530

    mma gemm naive fp16

--- inc/util.cuh
+++ inc/util.cuh
@@ -8,8 +8,8 @@
 #include <iomanip>
 #include <string>
 
-#define M 2048
-#define N 2048
+#define M 1024
+#define N 1024
 #define K 1024
 
 #define CUDA_CHECK(err)                                                        \
@@ -22,11 +22,36 @@
     }                                                                          \
   }
 
+bool verify_cpu(half* kernel_out , half* matrix_a ,half* matrix_b){
+  float* c = new float[M*N];
+  std::fill(c, c + M * N, 0.0f);
+
+  for(int row = 0 ; row < M ; row++){
+    for(int col = 0 ; col < N ; col++){
+      float acc= 0.0f;
+      for(int k = 0 ; k < K ; k++){
+        acc += __half2float(matrix_a[row*K + k] * matrix_b[k * N + col]);
+      }
+      c[row * N + col] = acc;
+    }
+  }
+
+  for(int i = 0 ; i < M * N ; i++){
+    if(std::fabs(__half2float(kernel_out[i]) - c[i]) > 1e-2){
+      std::cout << __half2float(kernel_out[i]) << " " << c[i];
+      return false;
+    }
+  }
+  return true;
+}
+
+
 template <class dtype1 , class dtype2>
 bool verify(dtype1 *kernel_out, dtype2 *cublas_out, int size) {
+  
 
   for (int i = 0; i < size; i++) {
-    if (std::fabs(static_cast<float>(kernel_out[i]) - static_cast<float>(cublas_out[i])) > 1e-3) {
+    if (std::fabs(static_cast<float>(kernel_out[i]) - static_cast<float>(cublas_out[i])) > 1){
       std::cout << static_cast<float>(kernel_out[i]) << " " << static_cast<float>(cublas_out[i]) << std::endl;
       // printf("kernel out : %f  || cublas out : %f"  , kernel_out[i] , cublas_out[i]);
       return false;
@@ -41,7 +66,7 @@ template <class dtype>
 void init(dtype *arr, int size) {
   for (int i = 0; i < size; i++) {
     // arr[i] = static_cast<float>(std::rand()) / (static_cast<float>(RAND_MAX) + 1.0f);
-    arr[i]= i / size;
+    arr[i]= float(i) / size;
   }
 }
 
@@ -57,9 +82,7 @@ void launch_cublass_fp32(float *ha, float *hb, float *hc,
 
   float alpha = 1.0f;
   float beta = 0.0f;
-  // cublasSetMatrix(M, K, sizeof(float), ha, M, da, M);
-  // cublasSetMatrix(K, N, sizeof(float), hb, K, db, K);
-  // cublasSetMatrix(M, N, sizeof(float), hc, M, dc, M);
+  
 
   int lda = K;
   int ldb = N;
@@ -96,35 +119,56 @@ void launch_cublass_fp32(float *ha, float *hb, float *hc,
   cublasDestroy(handle);
 }
 
-void launch_cublass_fp16(half *ha, half *hb, half *hc, half *da, half *db,
-                         half *dc) {
 
+void launch_cublass_fp16(__half* da , __half* db , __half* dc){
+  /*
+  Row major cublas with dtype fp16 using tensor cores
+  by defualt cublas is column major hence implements 
+  C(nxm) = B(nxk) . A(kxm)
+  so we need to implement 
+  C^T = B^T . A^T
+  */
+  //create handle
   cublasHandle_t handle;
   cublasCreate(&handle);
 
+  //cuda events
   cudaEvent_t start, end;
   cudaEventCreate(&start);
   cudaEventCreate(&end);
 
-  half alpha = __float2half(1.0f);
-  half beta = __float2half(0.0f);
-  // cublasSetMatrix(M, K, sizeof(float), ha, M, da, M);
-  // cublasSetMatrix(K, N, sizeof(float), hb, K, db, K);
-  // cublasSetMatrix(M, N, sizeof(float), hc, M, dc, M);
+  __half alpha = __float2half(1.0f);
+  __half beta = __float2half(0.0f);
 
-  int lda = K;
-  int ldb = N;
-  int ldc = N;
-  // init run
+  cublasStatus_t status = cublasGemmEx(
+    handle, // handle
+    CUBLAS_OP_N, // no transpose for matrix b
+    CUBLAS_OP_N, // no transpose for matrix a
+    N, // cols of B and C
+    M, // rows of A and C
+    K, // common axis 
+    &alpha, 
+    db, // pointer to matrix B
+    CUDA_R_16F,  // float16
+    N, // leading dim of matrix B
+    da, // pointer to matrix A 
+    CUDA_R_16F,  // float16
+    K, // leading dim of matrix A
+    &beta, 
+    dc,  // pointer to matrix C
+    CUDA_R_16F,  // floa16 
+    N,  // leading dim of matrix C
+    CUBLAS_COMPUTE_16F, // accum dtype
+    CUBLAS_GEMM_DEFAULT_TENSOR_OP
+  );
 
-  cublasHgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, M, K, &alpha, db, ldb, da,
-              lda, &beta, dc, ldc);
   int iter = 100;
-
   cudaEventRecord(start);
   for (int i = 0; i < iter; i++) {
-    cublasHgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, M, K, &alpha, db, ldb, da,
-                lda, &beta, dc, ldc);
+    cublasStatus_t status =
+        cublasGemmEx(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, M, K, &alpha, db,
+                     CUDA_R_16F, N, da, CUDA_R_16F, K, &beta, dc, CUDA_R_16F, N,
+                     CUBLAS_COMPUTE_16F, CUBLAS_GEMM_DEFAULT_TENSOR_OP);
   }
 
   cudaEventRecord(end);
@@ -137,15 +181,10 @@ void launch_cublass_fp16(half *ha, half *hb, half *hc, half *da, half *db,
             << (ms / iter) / 1000 << std::endl;
 
   long long int FLOP = 2LL * M * N * K;
-  std::cout << "CUBLAS GFLOPS : " << std::fixed << std::setprecision(5)
-            << ((FLOP / ((ms / iter) / 1000))) / 1e9 << std::endl;
-
-  cublasGetMatrix(M, N, sizeof(float), dc, M, hc, M);
-  cudaFree(da);
-  cudaFree(db);
-  cudaFree(dc);
-  cublasDestroy(handle);
+  std::cout << "CUBLAS TFLOPS : " << std::fixed << std::setprecision(5)
+            << ((FLOP / ((ms / iter) / 1000))) / 1e12 << std::endl;
 }
+
 
 void benchmark_fp32(void (*func)(float *, float *, float *),
                std::string name) {
@@ -207,7 +246,7 @@ void benchmark_fp32(void (*func)(float *, float *, float *),
   std::cout << "\n"<<out << std::endl; 
 }
 
-void benchmark_fp16(void (*func)(half *, half *, half *), std::string name) {
+void benchmark_fp16(void (*func)(half *, half *, float *), std::string name) {
 
   half *ha = new half[M * K];
   half *hb = new half[K * N];
@@ -220,7 +259,8 @@ void benchmark_fp16(void (*func)(half *, half *, half *), std::string name) {
   std::fill(hc, hc + M * N, 0.0f);
   std::fill(hd, hd + M * N, 0.0f);
 
-  half *da, *db, *dc, *dd;
+  half *da, *db ,*dd;
+  float* dc;
   cudaMalloc(&da, sizeof(half) * M * K);
   cudaMalloc(&db, sizeof(half) * K * N);
   cudaMalloc(&dc, sizeof(float) * M * N); // for kernel
@@ -251,17 +291,18 @@ void benchmark_fp16(void (*func)(half *, half *, half *), std::string name) {
             << (ms / ITER) / 1000 << std::endl;
 
   long long int FLOP = 2LL * M * N * K;
-  std::cout << name << " GFLOPS : " << std::fixed << std::setprecision(5)
-            << ((FLOP / ((ms / ITER) / 1000))) / 1e9 << std::endl;
+  std::cout << name << " TFLOPS : " << std::fixed << std::setprecision(5)
+            << ((FLOP / ((ms / ITER) / 1000))) / 1e12 << std::endl;
 
   std::cout << "------------------------" << std ::endl;
 
-  launch_cublass_fp16(ha, hb, hd, da, db, dd);
+  launch_cublass_fp16(da, db, dd);
 
-  cudaMemcpy(hc, dc, sizeof(float) * M * N, cudaMemcpyDeviceToHost);
-  cudaMemcpy(hd, dd, sizeof(float) * M * N, cudaMemcpyDeviceToHost);
-
-  bool ans = verify(hd, hc, M * N);
+  cudaMemcpy(hc, dc, sizeof(float) * M * N, cudaMemcpyDeviceToHost); // kernel
+  cudaMemcpy(hd, dd, sizeof(half) * M * N, cudaMemcpyDeviceToHost); // cublas
+  // std::cout << __half2float(hd[10])  << std::endl;
+  // bool ans = verify(hc, hd, M * N);
+  bool ans = verify_cpu(hd , ha , hb);
   std::string out = ans ? "PASS" : "FAIL";
   std::cout << "\n" << out << std::endl;
 }
---
+++ src/mma_gemm_naive_fp16.cu
@@ -0,0 +1,84 @@
+#include <cuda_runtime.h>
+#include <mma.h>
+#include <cuda_fp16.h>
+
+#include "../inc/util.cuh"
+using namespace nvcuda;
+
+const int WMMA_M = 16;
+const int WMMA_N = 16;
+const int WMMA_K = 16;    
+
+#define BLOCK_SIZE 128
+#define WARP_SIZE 32
+
+__global__ void wmma_naive_gemm(half *a, half *b, float *c, float alpha,
+                                float beta) {
+
+  int lda = K; 
+  int ldb = N;   
+  int ldc = N; 
+
+  int warpM = (blockIdx.x * blockDim.x + threadIdx.x) / warpSize; // 0 , 1
+  int warpN = (blockIdx.y * blockDim.y + threadIdx.y);
+
+  //declare the fragements 
+  wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> a_frag;
+  wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::col_major> b_frag;
+  wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> acc_frag;
+  wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> c_frag;
+
+  wmma::fill_fragment(acc_frag, 0.0f);  
+
+  for (int i = 0; i < K; i += WMMA_K) {
+    int aRow = warpM * WMMA_M; 
+    int aCol = i;
+
+    int bRow = i;
+    int bCol = warpN * WMMA_N;
+
+    if (aRow < M && aCol < K && bRow < K && bCol < N) {
+      // Row-major addressing: A[row][col] = A[row * lda + col]
+      wmma::load_matrix_sync(a_frag, a + aRow * lda + aCol, lda);
+      wmma::load_matrix_sync(b_frag, b + bRow * ldb + bCol, ldb);
+
+      wmma::mma_sync(acc_frag, a_frag, b_frag, acc_frag);
+    }
+  }
+  
+  int cRow = warpM * WMMA_M;
+  int cCol = warpN * WMMA_N;
+
+  if (cRow < M && cCol < N) {
+    // wmma::load_matrix_sync(c_frag, c + cRow * ldc + cCol, ldc,
+    //                        wmma::mem_row_major);
+
+    #pragma unroll
+    for (int i = 0; i < c_frag.num_elements; i++) {
+      c_frag.x[i] = acc_frag.x[i];
+    }
+    
+    wmma::store_matrix_sync(c + cRow * ldc + cCol, c_frag, ldc,
+                            wmma::mem_row_major);
+  }
+}
+
+
+void launch_wmma_kernel(
+    half* matrix_a,
+    half* matrix_b,
+    float* matrix_out
+){
+    dim3 block_dim(32 , 4);
+    dim3 grid_dim(
+        (M + WMMA_M - 1) / WMMA_M / (block_dim.x / WARP_SIZE),
+        (N + WMMA_N - 1) / WMMA_N / block_dim.y
+    );
+    wmma_naive_gemm<<<grid_dim , block_dim>>>(matrix_a , matrix_b , matrix_out , 1 , 0);
+
+
+}
+
+int main(){    
+    benchmark_fp16(launch_wmma_kernel, "WMMA_2 NAIVE ");
+}
