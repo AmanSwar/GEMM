b2d5e282228946cc25acbe5cf1afd461c82f653c
Aman <aman.swar001@gmail.com>
2025-09-26 11:19:16 +0530

    feat : add div_ceil host inline function

--- src/mma/common.cuh
+++ src/mma/common.cuh
@@ -3,6 +3,13 @@
 #include <cuda_runtime.h>
 #include <cuda_fp16.h>
 
+
+
 #define LDST32BITS(value) (reinterpret_cast<half2 *>(&(value))[0])
 #define LDST64BITS(value) (reinterpret_cast<float2*>(&(value))[0])
 #define LDST128BITS(value) (reinterpret_cast<float4*>(&(value))[0])
+
+// math utils
+__device__ __host__ __forceinline__ int div_ceil(int a, int b) {
+  return (a % b != 0) ? (a / b + 1) : (a / b);
+}
