858e67d2db0b86f2e8878da2074ce80cba0116b3
Aman <aman.swar001@gmail.com>
2025-09-27 16:08:14 +0530

    add half mma ptx

--- src/mma/common.cuh
+++ src/mma/common.cuh
@@ -44,13 +44,23 @@
       : "=r"(R0), "=r"(R1)                                                     \
       : "r"(addr))
 
-      
+
 #define LDMATRIX_X4_T(R0, R1, R2, R3, addr)                                    \
   asm volatile(                                                                \
       "ldmatrix.sync.aligned.x4.trans.m8n8.shared.b16 {%0, %1, %2, %3}, "      \
       "[%4];\n"                                                                \
       : "=r"(R0), "=r"(R1), "=r"(R2), "=r"(R3)                                 \
       : "r"(addr))
+
+
+// half precision mma instruction (16x8x16)
+#define HMMA16816(RD0, RD1, RA0, RA1, RA2, RA3, RB0, RB1, RC0, RC1)            \
+  asm volatile(                                                                \
+      "mma.sync.aligned.m16n8k16.row.col.f16.f16.f16.f16 {%0, %1}, {%2, %3, "  \
+      "%4, %5}, {%6, %7}, {%8, %9};\n"                                         \
+      : "=r"(RD0), "=r"(RD1)                                                   \
+      : "r"(RA0), "r"(RA1), "r"(RA2), "r"(RA3), "r"(RB0), "r"(RB1), "r"(RC0),  \
+        "r"(RC1))
 
 // math utils
 __device__ __host__ __forceinline__ int div_ceil(int a, int b) {
